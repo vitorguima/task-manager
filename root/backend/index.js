5869e29ccff9a0b985e4df99bc6e1dc858ca57ba
Vitor <vitorguima@gmail.com>
2021-11-10 08:53:43 -0300

    add procfile and .env configs

--- root/backend/index.js
+++ root/backend/index.js
@@ -22,7 +22,7 @@ require('dotenv').config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.APP_URL,
   credentials: true,
 }));  
 
